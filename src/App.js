060212516cfa84f0d85c1bc0e93f9c384badb9dc
Thatiana Emanuelle <thatiana@yahoo.com.br>
2018-10-31 13:18:16 -0300

    entrega

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Card from './Card.js';
+import ArtistCard from './ArtistCard.js';
+import { Route, Link } from 'react-router-dom';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      artists:[]
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount(){
+    const BASE_URL = 'https://peaceful-badlands-98440.herokuapp.com';
+    const BASE_OPTIONS = {
+
+    }
+    const options = {
+      method: 'post',
+      headers:{'Content-Type': 'application/json'},
+      credentials: 'include',
+      body: JSON.stringify({email: 'rafael@laboratoria.la', password: 'banana'})
+    };
+
+    fetch(`${BASE_URL}/login`, options)
+      // .then(data => console.log(data));
+      .then(res => {
+        const options = {
+          method: 'get',
+          credentials: 'include',
+        };
+        fetch(`${BASE_URL}/artists`, options)
+          .then(res => res.json())
+          .then(data => this.setState({ artists: data }));
+      })
+  }
+
+  handleClick(){
+  }
+  render() {
+    return (
+      <div>
+        <nav className="menu">
+          <Link to="/">Home</Link>
+          <Link to="/artists">Artistas</Link>
+          <Link to="/about">Sobre nós</Link>
+          <Link to="/contato">Entre em Contato</Link>
+        </nav>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About}/>
+        <Route path="/artists" render={ () =>
+          <div>
+            {this.state.artists.map(artist => <ArtistCard {...artist} key={artist.id} />)}
+          </div>
+        } />
+
+        <Route path="/about/yes" render={() => "Isso mesmo " } />
+        <Route path="/about/:id" render={({match}) => "O id veio " + match.params.id } />
+
+      </div>
+    );
+  }
+}
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home legal</h2>
+      <p>Bem vinda a nossa grande maravilhosa e incrível app de react</p>
+    </div>
+  )
+}
+
+const About = () => {
+  return (
+    <div>
+      <h1>legal</h1>
+      Nós somos demais.
+      <Link to="/about/yes">clica aqui</Link>
+
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+export default App;
---
+++ src/ArtistCard.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Card from './Card.js';
+import './ArtistCard.css';
+import PropTypes from 'prop-types';
+
+function ArtistTrack(props){
+  return(
+  <li className={props.className}>
+    {props.title} < br/>
+    <iframe src= {props.url}/>
+  </li>)
+}
+
+ArtistTrack.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  className: PropTypes.string
+}
+
+class ArtistCard extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {tracks: []}
+    this.getArtistTracks = this.getArtistTracks.bind(this);
+  }
+
+  getArtistTracks(event){
+    const BASE_URL = 'https://peaceful-badlands-98440.herokuapp.com';
+    const options = {
+      method: 'get',
+      credentials: 'include',
+    };
+    fetch(`${BASE_URL}/artists/${this.props.id}/tracks`, options)
+    .then(res => res.json())
+    .then(data => {
+      if(data.length === 0){
+        this.setState({ tracks: [{title:'Não tem nada'}] })
+      }else{
+        this.setState({ tracks: data })
+      }
+    });
+    // alert(this.props.id);
+  }
+
+  render(){
+    return(
+      <Card>
+        <h2>{this.props.name}</h2>
+        <h3>{this.props.genre}</h3>
+        <button onClick={this.getArtistTracks}>ver musicas</button>
+        <ul>
+          {this.state.tracks.map((track, index) => <ArtistTrack key={index} {...track}/>)}
+        </ul>
+      </Card>
+    )
+  }
+}
+
+ArtistCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired
+}
+
+export default ArtistCard;
+
